1bfa2db1be07375ecf8ac61695414580de701575
Your Name <you@example.com>
2019-05-14 13:38:06 -1000

    yes

--- platform.js
+++ platform.js
@@ -1,7 +1,7 @@
 function platform() {
     this.height = 100;
     this.width = 100;
-    this.x = Math.floor((Math.random() * w/1.7) + w/10);
+    this.x = w/2;
     this.y = Math.floor((Math.random() * h/1.7 ) +h/10);
     this.hit = 0;
     this.show = function () {
